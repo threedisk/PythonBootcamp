8f52eb9b51df19f7d79f7add1bc4fe1a9e45e564
threedisk <threedisk@gmail.com>
2022-05-11 05:56:18 +0700

    day-002

---
+++ day-002/day-2-data-type.py
@@ -0,0 +1,17 @@
+# Data Types
+
+# String = dimulai dengan tanda petik dua
+# [] kurung kotak tersebut bermakna array, array selalu dimulai dari 0, bukan dari 1
+print("=============")
+print("Data Tipe String")
+print("=============")
+print("123"+"456") # Data tersebut adalah data string, dikarenakan ada petik dua "" , jadi operasi didepan adalah seperti concatenate yakni menghasilkan 123456
+print("Hello"[0]) # Digunakan untuk membaca H saja pada print 
+print("Hello"[4]) # Digunakan untuk membaca O saja pada print
+
+
+#Integer
+print("=============")
+print("Data Tipe Integer")
+print("=============")
+print(123+456) # Data bertipe integer, akan menghasilkan penjumlahan 123 + 456 = yakni 579
---
+++ day-002/readme.md
@@ -0,0 +1,13 @@
+## Day 002
+
+### Yang dipelajari
+1. Tipe data : beberapa tipe data seperti string, integer, float dan boolean, dan masih banyak lagi
+2. 
+3. 
+4. 
+### Band Name Generator
+
+### Band Name Generator Code
+```
+kodingan kamu disini
+```
