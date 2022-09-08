de0136b108dff9593eb1e8d709bb50798cb43b34
Mas Tris <threedisk@gmail.com>
2022-09-08 18:21:42 +0700

    update

--- day-003/day-3-start.py
+++ day-003/day-3-start.py
@@ -1,8 +1,6 @@
-from string import hexdigits
-
-
 print("Welcome to Rollercoaster Game")
 height=int(input("What is your height in cm?"))
+bill=0
 
 if height >= 120: # Jika tinggi badan lebih dari atau sama dengan 120
     print(f"Your height is {height} cm and you can ride the rollercoaster")
@@ -10,10 +8,21 @@ if height >= 120: # Jika tinggi badan lebih dari atau sama dengan 120
     # Kode dibawah ini adalah nested if, karena ada kondisi if didalam if
     age = int(input("What is your age? "))
     if (age >= 12 and age <=18 ) :
+        bill=7
         print("Please pay 7$")
     elif age < 12 :
+        bill=5
         print("Please pay 5$")
     else:
+        bill=12
         print("Please pay 12$")
+    
+    wants_photo=input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y" :
+        #Add 3$ bill
+        bill+=3
+    
+    print(f"Your bill is {bill}")
+
 else:
     print(f"Your height is {height} cm, sorry you can't ride rollercoaster")
--- day-003/readme.md
+++ day-003/readme.md
@@ -3,7 +3,8 @@
 ### Yang dipelajari
 1. Kondisi if-else dan operator perbandingan seperti > (lebih dari) , < (kurang dari), <= (kurang dari sama dengan ), >= (lebih dari sama dengan ), == (sama dengan), != ( tidak sama dengan)
 2. Ganjil atau Genap dengan menggunakan modulo (sisa pembagian, menggunakan simbol  % ). Contoh 7 % 2 akan menghasilkan 1, 7 % 3 juga akan menghasilkan angka 1
-3. Nested if statement and elif statement ( Nested if dan elif )
+3. Nested if statement and elif statement ( Nested if dan elif ) serta multiple if statement
+
 
 
 ### Adventure Game
